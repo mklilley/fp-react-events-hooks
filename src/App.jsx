113b17281f0ad5915383293544cf2ba5a6800b1b
Matt Lilley <m.k.lilley@gmail.com>
2022-08-23 13:06:56 +0100

    Adds example code for events and effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React, { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [students, setStudents] = useState([])
+  const [cohort, setCohort] = useState("bhatia")
+  const [inputValue, setInputValue] = useState("")
 
-  function increaseSize(e,size){
-    e.target.style.fontSize = size
+  function handleInput(e){
+    setInputValue(e.target.value.toLowerCase())
   }
 
-  function resetSize(e){
-    e.target.style.fontSize = "1rem"
+  function handleSubmit(e){
+    e.preventDefault();
+    setCohort(inputValue)
   }
+
+  useEffect(()=>{
+     async function fetchData(){
+      const data = await fetch(`https://raw.githubusercontent.com/getfutureproof/fp_study_notes_hello_github/main/${cohort}/roster.json`)
+      const json = await data.json()
+      setStudents(json.students)
+    }
+    fetchData()
+  }, [cohort])
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onMouseEnter={(e) => increaseSize(e,"2em")} onMouseLeave= {resetSize} onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{cohort}</h1>
+      {students.map(s => <p key={s.name}> {s.name}</p>)}
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleInput} value={inputValue}/>
+      </form>
     </div>
   )
 }
